7d12a295340df286f01a9aff3042bee6a963f21c
Jakub <jakub@example.com>
2018-04-25 18:05:56 +0200

    Create renderAlbums function, that retrieves album property (title) from a JSON fetched before. In the text tag there is also a key method because of performance reasons. Render list of albums.

--- src/components/MusicList.js
+++ src/components/MusicList.js
@@ -9,11 +9,15 @@ class MusicList extends Component {
     axios.get('https://rallycoding.herokuapp.com/api/music_albums')
       .then(response => this.setState({ albums: response.data }))
   }
+
+  renderAlbums() {
+    return this.state.albums.map(album => <Text key={album.title}>{album.title}</Text>)
+  }
   render() {
     console.log(this.state);
 
     return <View>
-      <Text>MusicList</Text>
+      { this.renderAlbums() }
     </View>
   }
 }
