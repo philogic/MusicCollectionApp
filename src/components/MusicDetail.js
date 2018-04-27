fed796ced6708edc736b7d3b2e98ca7ccc5f2105
Jakub <jakub@example.com>
2018-04-28 01:58:51 +0200

    Devide CaedItem Header into two sections for image and for text and style them appropriately. Destructure props to music parameter in order to refactor the code.

--- src/components/MusicDetail.js
+++ src/components/MusicDetail.js
@@ -1,16 +1,26 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardItem from './CardItem';
 
-const MusicDetail = (props) => {
+const MusicDetail = ({ music }) => {
+  const { title, artist, thumbnail_image } = music;
+  const { imageStyle,
+          imageContainerStyle,
+          headerContentStyle,
+          headerTextStyle
+  } = styles;
   return (
     <Card>
       <CardItem>
-        <View/>
-        <View style={styles.headerContentStyle}>
-          <Text>{props.music.title}</Text>
-          <Text>{props.music.artist}</Text>
+        <View style={imageContainerStyle}>
+          <Image
+            style={imageStyle}
+            source={{ uri: thumbnail_image }}/>
+        </View>
+        <View style={headerContentStyle}>
+          <Text style={headerTextStyle}>{title}</Text>
+          <Text>{artist}</Text>
         </View>
       </CardItem>
     </Card>
@@ -21,6 +31,19 @@ const styles = {
   headerContentStyle: {
     flexDirection: 'column',
     justifyContent: 'space-around'
+  },
+  headerTextStyle: {
+    fontSize: 18
+  },
+  imageStyle: {
+    height: 50,
+    width: 50
+  },
+  imageContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 15,
+    marginRight: 15,
   }
 };
 
