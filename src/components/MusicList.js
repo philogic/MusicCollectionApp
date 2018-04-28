cfca3f19807da0da255c18102cf3b9cddaec4099
Jakub <jakub@example.com>
2018-04-28 02:40:31 +0200

    Add an album cover as a subsequent element behind the header. Style it use flex to make width from left to right on every screen (height - hard coded) Enable scrolling: add ScrollView component and wrap MusicList with it (instead of View). Add flex: 1 style to index.js to make scrolling more efficient.

--- src/components/MusicList.js
+++ src/components/MusicList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { ScrollView } from 'react-native';
 import axios from 'axios';
 import MusicDetail from './MusicDetail'
 
@@ -17,9 +17,9 @@ class MusicList extends Component {
   render() {
     console.log(this.state);
 
-    return <View>
+    return <ScrollView>
       { this.renderAlbums() }
-    </View>
+    </ScrollView>
   }
 }
 
